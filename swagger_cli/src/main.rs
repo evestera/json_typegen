0f3e9cc111d72d40bec68f78557ddabae366736c
Erik Vesteraas <erik@vestera.as>
2017-02-10 14:41:21 +0100

    CLI: Format output with rustfmt

--- swagger_cli/src/main.rs
+++ swagger_cli/src/main.rs
@@ -1,15 +1,25 @@
 extern crate swagger_shared;
-
-use swagger_shared::{codegen_from_spec, SpecSource};
+extern crate rustfmt;
 
 use std::env;
+use swagger_shared::{codegen_from_spec, SpecSource};
+use rustfmt::config::{Config, WriteMode};
 
 fn main() {
+    // TODO: Add proper arg parsing and more configuration
+    // - At least: Input, output, name
     match env::args().skip(1).next() {
         Some(str) => {
-            let tokens = codegen_from_spec("Foobar", SpecSource::File(&str));
-            println!("{}", tokens.unwrap());
-        },
+            let tokens = codegen_from_spec("SwaggerApi", SpecSource::File(&str)).unwrap();
+            let mut code = String::from("pub struct SwaggerApi;");
+            code += tokens.as_str();
+
+            let input = rustfmt::Input::Text(code);
+            let mut output = std::io::stdout();
+            let mut config = Config::default();
+            config.write_mode = WriteMode::Plain;
+            rustfmt::format_input(input, &config, Some(&mut output)).unwrap();
+        }
         None => {
             println!("Usage: rs-swagger <json spec file>");
         }
