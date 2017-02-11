2f77d1f5845e0577de8894385aa04ffa9f4c3404
Erik Vesteraas <erik@vestera.as>
2017-02-11 14:04:16 +0100

    Expand spec with refs and schema. Start handling.

--- swagger_shared/src/lib.rs
+++ swagger_shared/src/lib.rs
@@ -6,15 +6,18 @@ extern crate serde_json;
 extern crate serde_derive;
 
 use std::fs::File;
-use std::io::Read;
+use std::collections::{ HashMap, HashSet };
 
-mod spec;
+pub mod spec;
+mod util;
 
 use spec::*;
+use util::*;
 
 pub enum SpecSource<'a> {
     Url(&'a str),
     File(&'a str),
+    Text(&'a str),
 }
 
 #[derive(Debug)]
@@ -22,7 +25,7 @@ pub enum SpecError {
     ReqwestError(reqwest::Error),
     JsonError(serde_json::Error),
     IoError(std::io::Error),
-    MissingSource
+    MissingSource,
 }
 
 impl From<reqwest::Error> for SpecError {
@@ -48,8 +51,9 @@ pub fn codegen_from_spec(name: &str, source: SpecSource) -> Result<quote::Tokens
     let name_id = quote::Ident::new(name);
 
     let paths = spec.paths;
+    let mut ctxt = Ctxt::new();
     if let Some(definitions) = spec.definitions {
-        println!("We have some definitions");
+        process_refmap(&definitions, "#/definitions", &mut ctxt);
     }
 
     let fns = generate_fns(vec!["foo", "bar", "baz"]);
@@ -60,6 +64,92 @@ pub fn codegen_from_spec(name: &str, source: SpecSource) -> Result<quote::Tokens
         }
     };
     Ok(tokens)
+}
+
+enum TypeDef {
+    Struct { name: String, fields: Vec<Field> },
+    Enum { name: String, variants: Vec<String> },
+    Alias { name: String, typ: String },
+}
+
+struct Field {
+    name: String,
+    original_name: Option<String>,
+    typ: String
+}
+
+struct Ctxt {
+    /// Map from spec path to type definitions
+    types: HashMap<String, TypeDef>,
+    /// Set of type names that are taken (same names as `types[x].name`)
+    names: HashSet<String>,
+    /// Should generated types with fields not marked as required be wrapped with Option?
+    wrap_optional: bool
+}
+
+impl Ctxt {
+    fn new() -> Self {
+        Ctxt {
+            types: HashMap::new(),
+            names: HashSet::new(),
+            wrap_optional: true
+        }
+    }
+}
+
+fn process_refmap(map: &HashMap<String, RefOr<SchemaObject>>,
+                  current_path: &str,
+                  ctxt: &mut Ctxt) -> Vec<Field> {
+    let mut temp_path: String = String::from(current_path) + "/";
+    let mut fields = Vec::new();
+
+    for (name, ref_or_schema) in map.iter() {
+        temp_path += name;
+        let field_type = process_ref_or_schema(ref_or_schema, &temp_path, ctxt);
+        let field_name = snake_case(name);
+        let original_name = if &field_name != name { Some(name.clone()) } else { None };
+        fields.push(Field {
+            name: field_name,
+            original_name: original_name,
+            typ: field_type
+        });
+        temp_path.truncate(current_path.len() + 1);
+    }
+
+    fields
+}
+
+fn process_ref_or_schema(ref_or_schema: &RefOr<SchemaObject>,
+                         current_path: &str,
+                         ctxt: &mut Ctxt) -> String {
+    match *ref_or_schema {
+        RefOr::Ref { ref path } => {
+            if !path.starts_with("#/definitions/") {
+                panic!("Unsupported $ref target: {} {}", current_path, path);
+            }
+            // let def = TypeDef::Alias { name: type_name, typ: target_name };
+            // ctxt.names.insert(def.name.clone());
+            // ctxt.types.insert(current_path, def);
+            typename_from_path(&path)
+        },
+        RefOr::Other(ref schema) => {
+            process_schema(&schema, &current_path, ctxt)
+        }
+    }
+}
+
+fn process_schema(schema: &SchemaObject,
+                  current_path: &str,
+                  ctxt: &mut Ctxt) -> String {
+    String::from("Value")
+}
+
+fn typename_from_path(path: &str) -> String {
+    if path.starts_with("#/definitions/") {
+        camel_case(&path[14..])
+    } else {
+        camel_case(&path)
+    }
 }
 
 fn generate_fns(names: Vec<&str>) -> Vec<quote::Tokens> {
@@ -76,15 +166,10 @@ fn generate_fns(names: Vec<&str>) -> Vec<quote::Tokens> {
 }
 
 fn get_spec(source: SpecSource) -> Result<Spec, SpecError> {
-    match source {
-        SpecSource::Url(url) => read_spec(reqwest::get(url)?),
-        SpecSource::File(path) => read_spec(File::open(path)?)
-    }
+    let parse_result = match source {
+        SpecSource::Url(url) => serde_json::de::from_reader(reqwest::get(url)?),
+        SpecSource::File(path) => serde_json::de::from_reader(File::open(path)?),
+        SpecSource::Text(text) => serde_json::from_str(text),
+    };
+    parse_result.map_err(SpecError::JsonError)
 }
-
-fn read_spec<T: Read>(reader: T) -> Result<Spec, SpecError> {
-    let spec: Spec = serde_json::de::from_reader(reader)?;
-    Ok(spec)
-}
-
-
--- swagger_shared/src/spec.rs
+++ swagger_shared/src/spec.rs
@@ -1,25 +1,26 @@
-use serde_json::value::{Value, Map};
+pub use serde_json::value::Value;
+use std::collections::HashMap;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Spec {
     pub swagger: String,
     pub info: Info,
     pub host: Option<String>,
     #[serde(rename = "basePath")]
     pub base_path: Option<String>,
-    pub paths: Map<String, Value>,
-    pub definitions: Option<Map<String, Value>>,
-    pub parameters: Option<Map<String, Value>>,
+    pub paths: HashMap<String, Value>,
+    pub definitions: Option<HashMap<String, RefOr<SchemaObject>>>,
+    pub parameters: Option<HashMap<String, Value>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Info {
     pub title: String,
     pub version: String,
     pub description: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct PathItem {
     #[serde(rename = "$ref")]
     pub ref_path: Option<String>,
@@ -34,5 +35,30 @@ pub struct PathItem {
     pub parameters: Vec<Value>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Operation {}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum RefOr<T> {
+    Ref {
+        #[serde(rename = "$ref")]
+        path: String
+    },
+    Other(T)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SchemaObject {
+    #[serde(rename = "type")]
+    pub typ: Option<String>, // Can actually also be array of strings
+    #[serde(default)]
+    pub required: Vec<String>,
+    #[serde(default)]
+    pub properties: HashMap<String, RefOr<SchemaObject>>,
+    pub items: Option<Box<RefOr<SchemaObject>>>,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    #[serde(rename = "enum")]
+    pub enumerated: Option<Vec<Value>> // The items can be any type
+}
