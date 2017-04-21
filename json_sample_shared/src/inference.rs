e11cdb9f4f2cd40f3744b11155f6ce553634a82d
Erik Vesteraas <erik@vestera.as>
2017-04-21 08:40:54 +0200

    shared: Move inference code to its own file

---
+++ json_sample_shared/src/inference.rs
@@ -0,0 +1,166 @@
+use linked_hash_map::LinkedHashMap;
+use serde_json::{ Value, Map };
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum InferredType {
+    Null,
+    Any,
+    Bool,
+    StringT,
+    Integer,
+    Floating,
+    EmptyVec,
+    VecT { elem_type: Box<InferredType> },
+    Struct { fields: LinkedHashMap<String, InferredType> },
+    Optional(Box<InferredType>)
+}
+
+fn unify(a: InferredType, b: InferredType) -> InferredType {
+    if a == b {
+        return a;
+    }
+    use self::InferredType::*;
+    match (a, b) {
+        (Integer, Floating) |
+        (Floating, Integer) => Floating,
+        (a, Null) | (Null, a) => make_optional(a),
+        (a, Optional(b)) | (Optional(b), a) => make_optional(unify(a, *b)),
+        (EmptyVec, VecT { elem_type: e }) |
+        (VecT { elem_type: e }, EmptyVec) => VecT { elem_type: e },
+        (VecT { elem_type: e1 }, VecT { elem_type: e2 }) => {
+            VecT { elem_type: Box::new(unify(*e1, *e2)) }
+        }
+        (Struct { fields: f1 }, Struct { fields: f2 }) => {
+            Struct { fields: unify_struct_fields(f1, f2) }
+        }
+        _ => Any,
+    }
+}
+
+fn make_optional(a: InferredType) -> InferredType {
+    use self::InferredType::*;
+    match a {
+        Null | Any | Optional(_) => a,
+        non_nullable => Optional(Box::new(non_nullable)),
+    }
+}
+
+fn unify_struct_fields(f1: LinkedHashMap<String, InferredType>,
+                       mut f2: LinkedHashMap<String, InferredType>)
+                       -> LinkedHashMap<String, InferredType> {
+    if f1 == f2 {
+        return f1;
+    }
+    let mut unified = LinkedHashMap::new();
+    for (key, val) in f1.into_iter() {
+        match f2.remove(&key) {
+            Some(val2) => {
+                unified.insert(key, unify(val, val2));
+            },
+            None => {
+                unified.insert(key, make_optional(val));
+            }
+        }
+    }
+    for (key, val) in f2.into_iter() {
+        unified.insert(key, make_optional(val));
+    }
+    unified
+}
+
+pub fn infer_type_from_value(value: &Value) -> InferredType {
+    match *value {
+        Value::Null => InferredType::Null,
+        Value::Bool(_) => InferredType::Bool,
+        Value::Number(ref n) => {
+            if n.is_i64() {
+                InferredType::Integer
+            } else {
+                InferredType::Floating
+            }
+        },
+        Value::String(_) => InferredType::StringT,
+        Value::Array(ref values) => {
+            infer_type_for_array(values)
+        },
+        Value::Object(ref map) => {
+            InferredType::Struct { fields: infer_types_for_fields(map) }
+        }
+    }
+}
+
+fn infer_type_for_array(values: &[Value]) -> InferredType {
+    match values.split_first() {
+        None => InferredType::EmptyVec,
+        Some((first, rest)) => {
+            let first_type = infer_type_from_value(first);
+            let inner = rest.iter().fold(first_type, |typ, val| {
+                let new_type = infer_type_from_value(val);
+                unify(typ, new_type)
+            });
+            InferredType::VecT { elem_type: Box::new(inner) }
+        }
+    }
+}
+
+fn infer_types_for_fields(map: &Map<String, Value>) -> LinkedHashMap<String, InferredType> {
+    map.iter()
+        .map(|(name, value)| (name.clone(), infer_type_from_value(value)))
+        .collect()
+}
+
+#[test]
+fn test_unify() {
+    use InferredType::*;
+    assert_eq!(unify(Bool, Bool), Bool);
+    assert_eq!(unify(Bool, Integer), Any);
+    assert_eq!(unify(Integer, Floating), Floating);
+    assert_eq!(unify(Null, Any), Any);
+    assert_eq!(unify(Null, Bool), Optional(Box::new(Bool)));
+    assert_eq!(unify(Null, Optional(Box::new(Integer))), Optional(Box::new(Integer)));
+    assert_eq!(unify(Any, Optional(Box::new(Integer))), Any);
+    assert_eq!(unify(Any, Optional(Box::new(Integer))), Any);
+    assert_eq!(unify(Optional(Box::new(Integer)), Optional(Box::new(Floating))),
+               Optional(Box::new(Floating)));
+    assert_eq!(unify(Optional(Box::new(StringT)), Optional(Box::new(Integer))), Any);
+}
+
+// based on hashmap! macro from maplit crate
+macro_rules! string_hashmap {
+    ($($key:expr => $value:expr,)+) => { string_hashmap!($($key => $value),+) };
+    ($($key:expr => $value:expr),*) => {
+        {
+            let mut _map = ::linked_hash_map::LinkedHashMap::new();
+            $(
+                _map.insert($key.to_string(), $value);
+            )*
+            _map
+        }
+    };
+}
+
+#[test]
+fn test_unify_struct_fields() {
+    use InferredType::*;
+    {
+        let f1 = string_hashmap!{
+            "a" => Integer,
+            "b" => Bool,
+            "c" => Integer,
+            "d" => StringT,
+        };
+        let f2 = string_hashmap!{
+            "a" => Integer,
+            "c" => Floating,
+            "d" => Null,
+            "e" => Any,
+        };
+        assert_eq!(unify_struct_fields(f1, f2), string_hashmap!{
+            "a" => Integer,
+            "b" => Optional(Box::new(Bool)),
+            "c" => Floating,
+            "d" => Optional(Box::new(StringT)),
+            "e" => Any,
+        });
+    }
+}
--- json_sample_shared/src/lib.rs
+++ json_sample_shared/src/lib.rs
@@ -9,15 +9,17 @@ extern crate lazy_static;
 extern crate linked_hash_map;
 
 use std::fs::File;
-use serde_json::{ Value, Map };
+use serde_json::{ Value };
 use std::collections::{ HashSet };
 use quote::{ Tokens, Ident, ToTokens };
 use std::ascii::AsciiExt;
 use linked_hash_map::LinkedHashMap;
 
 mod util;
+mod inference;
 
 use util::*;
+use inference::*;
 
 mod errors {
     error_chain! {
@@ -141,114 +143,6 @@ macro_rules! some_if {
     })
 }
 
-#[derive(Debug, PartialEq, Clone)]
-enum InferredType {
-    Null,
-    Any,
-    Bool,
-    StringT,
-    Integer,
-    Floating,
-    EmptyVec,
-    VecT { elem_type: Box<InferredType> },
-    Struct { fields: LinkedHashMap<String, InferredType> },
-    Optional(Box<InferredType>)
-}
-
-fn unify(a: InferredType, b: InferredType) -> InferredType {
-    if a == b {
-        return a;
-    }
-    use InferredType::*;
-    match (a, b) {
-        (Integer, Floating) |
-        (Floating, Integer) => Floating,
-        (a, Null) | (Null, a) => make_optional(a),
-        (a, Optional(b)) | (Optional(b), a) => make_optional(unify(a, *b)),
-        (EmptyVec, VecT { elem_type: e }) |
-        (VecT { elem_type: e }, EmptyVec) => VecT { elem_type: e },
-        (VecT { elem_type: e1 }, VecT { elem_type: e2 }) => {
-            VecT { elem_type: Box::new(unify(*e1, *e2)) }
-        }
-        (Struct { fields: f1 }, Struct { fields: f2 }) => {
-            Struct { fields: unify_struct_fields(f1, f2) }
-        }
-        _ => Any,
-    }
-}
-
-fn make_optional(a: InferredType) -> InferredType {
-    use InferredType::*;
-    match a {
-        Null | Any | Optional(_) => a,
-        non_nullable => Optional(Box::new(non_nullable)),
-    }
-}
-
-fn unify_struct_fields(f1: LinkedHashMap<String, InferredType>,
-                       mut f2: LinkedHashMap<String, InferredType>)
-                       -> LinkedHashMap<String, InferredType> {
-    if f1 == f2 {
-        return f1;
-    }
-    let mut unified = LinkedHashMap::new();
-    for (key, val) in f1.into_iter() {
-        match f2.remove(&key) {
-            Some(val2) => {
-                unified.insert(key, unify(val, val2));
-            },
-            None => {
-                unified.insert(key, make_optional(val));
-            }
-        }
-    }
-    for (key, val) in f2.into_iter() {
-        unified.insert(key, make_optional(val));
-    }
-    unified
-}
-
-fn infer_type_from_value(ctxt: &mut Ctxt, value: &Value) -> InferredType {
-    match *value {
-        Value::Null => InferredType::Null,
-        Value::Bool(_) => InferredType::Bool,
-        Value::Number(ref n) => {
-            if n.is_i64() {
-                InferredType::Integer
-            } else {
-                InferredType::Floating
-            }
-        },
-        Value::String(_) => InferredType::StringT,
-        Value::Array(ref values) => {
-            infer_type_for_array(ctxt, values)
-        },
-        Value::Object(ref map) => {
-            InferredType::Struct { fields: infer_types_for_fields(ctxt, map) }
-        }
-    }
-}
-
-fn infer_type_for_array(ctxt: &mut Ctxt, values: &[Value]) -> InferredType {
-    match values.split_first() {
-        None => InferredType::EmptyVec,
-        Some((first, rest)) => {
-            let first_type = infer_type_from_value(ctxt, first);
-            let inner = rest.iter().fold(first_type, |typ, val| {
-                let new_type = infer_type_from_value(ctxt, val);
-                unify(typ, new_type)
-            });
-            InferredType::VecT { elem_type: Box::new(inner) }
-        }
-    }
-}
-
-fn infer_types_for_fields(ctxt: &mut Ctxt, map: &Map<String, Value>) -> LinkedHashMap<String, InferredType> {
-    map.iter()
-        .map(|(name, value)| (name.clone(), infer_type_from_value(ctxt, value)))
-        .collect()
-}
-
 pub fn from_str_with_defaults(name: &str, json: &str) -> Result<Tokens> {
     codegen(name, &SampleSource::Text(json), Options::default())
 }
@@ -260,7 +154,7 @@ pub fn codegen(name: &str, source: &SampleSource, options: Options) -> Result<To
         type_names: HashSet::new(),
         types: Vec::new(),
     };
-    let inferred = infer_type_from_value(&mut ctxt, &sample);
+    let inferred = infer_type_from_value(&sample);
     let type_name = generate_type_from_inferred(&mut ctxt, name, &inferred);
 
     let example = some_if!(ctxt.options.runnable, {
@@ -448,60 +342,4 @@ fn get_and_parse_sample(source: &SampleSource) -> Result<Value> {
         SampleSource::Text(text) => serde_json::from_str(text),
     };
     Ok(parse_result.chain_err(|| "Unable to parse JSON sample")?)
-}
-
-#[test]
-fn test_unify() {
-    use InferredType::*;
-    assert_eq!(unify(Bool, Bool), Bool);
-    assert_eq!(unify(Bool, Integer), Any);
-    assert_eq!(unify(Integer, Floating), Floating);
-    assert_eq!(unify(Null, Any), Any);
-    assert_eq!(unify(Null, Bool), Optional(Box::new(Bool)));
-    assert_eq!(unify(Null, Optional(Box::new(Integer))), Optional(Box::new(Integer)));
-    assert_eq!(unify(Any, Optional(Box::new(Integer))), Any);
-    assert_eq!(unify(Any, Optional(Box::new(Integer))), Any);
-    assert_eq!(unify(Optional(Box::new(Integer)), Optional(Box::new(Floating))),
-               Optional(Box::new(Floating)));
-    assert_eq!(unify(Optional(Box::new(StringT)), Optional(Box::new(Integer))), Any);
-}
-
-// based on hashmap! macro from maplit crate
-macro_rules! string_hashmap {
-    ($($key:expr => $value:expr,)+) => { string_hashmap!($($key => $value),+) };
-    ($($key:expr => $value:expr),*) => {
-        {
-            let mut _map = ::linked_hash_map::LinkedHashMap::new();
-            $(
-                _map.insert($key.to_string(), $value);
-            )*
-            _map
-        }
-    };
-}
-
-#[test]
-fn test_unify_struct_fields() {
-    use InferredType::*;
-    {
-        let f1 = string_hashmap!{
-            "a" => Integer,
-            "b" => Bool,
-            "c" => Integer,
-            "d" => StringT,
-        };
-        let f2 = string_hashmap!{
-            "a" => Integer,
-            "c" => Floating,
-            "d" => Null,
-            "e" => Any,
-        };
-        assert_eq!(unify_struct_fields(f1, f2), string_hashmap!{
-            "a" => Integer,
-            "b" => Optional(Box::new(Bool)),
-            "c" => Floating,
-            "d" => Optional(Box::new(StringT)),
-            "e" => Any,
-        });
-    }
 }
