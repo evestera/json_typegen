2b184a7884b3f630654ae281870e20a18fb77bf1
Erik Vesteraas <erik@vestera.as>
2017-07-08 14:29:10 +0200

    shared: Add benchmarking code
    Just to keep an eye on things. Worst time so far for full codegen based
    on 2000 lines of JSON was 1ms.

---
+++ json_typegen_shared/benches/bench.rs
@@ -0,0 +1,25 @@
+#![cfg_attr(feature = "unstable", feature(test))]
+#![cfg(all(feature = "unstable", test))]
+
+// Benchmark currently only works on nightly Rust due to extern crate test
+// Tracking issue: https://github.com/rust-lang/rust/issues/29553
+
+extern crate json_typegen_shared;
+extern crate test;
+
+use json_typegen_shared::from_str_with_defaults;
+use test::Bencher;
+
+macro_rules! file_bench {
+    ($name:ident, $file_path:expr) => (
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            b.iter(|| {
+                from_str_with_defaults("Article", include_str!($file_path))
+            });
+        }
+    )
+}
+
+file_bench!(magic_card_list, "fixtures/magic_card_list.json");
+file_bench!(zalando_article, "fixtures/zalando_article.json");
