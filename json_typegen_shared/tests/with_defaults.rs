b894d8f6538ce60a942e75a95fd15ee5f8be589f
Erik Vesteraas <erik@vestera.as>
2017-07-08 13:25:46 +0200

    Add some code output tests

---
+++ json_typegen_shared/tests/with_defaults.rs
@@ -0,0 +1,160 @@
+extern crate json_typegen_shared;
+extern crate syn;
+
+use json_typegen_shared::from_str_with_defaults;
+
+/// Function to test AST equality, not string equality
+fn code_output_test(name: &str, input: &str, expected: &str) {
+    let res = from_str_with_defaults(name, input);
+    let output = res.unwrap();
+    assert_eq!(
+        syn::parse_items(output.as_ref()),
+        syn::parse_items(expected),
+        "\n\nUnexpected output code:\n  input: {}\n  output:\n{}\n  expected: {}",
+        input, output.as_ref(), expected);
+}
+
+#[test]
+fn empty_object() {
+    code_output_test(
+        "Root",
+        r##"
+            {}
+        "##,
+        r##"
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct Root {}
+        "##
+    );
+}
+
+#[test]
+fn point() {
+    code_output_test(
+        "Point",
+        r##"
+            {
+                "x": 2,
+                "y": 3
+            }
+        "##,
+        r##"
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct Point {
+                x: i64,
+                y: i64,
+            }
+        "##
+    );
+}
+
+#[test]
+fn pub_point() {
+    code_output_test(
+        "pub Point",
+        r##"
+            {
+                "x": 2,
+                "y": 3
+            }
+        "##,
+        r##"
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            pub struct Point {
+                pub x: i64,
+                pub y: i64,
+            }
+        "##
+    );
+}
+
+#[test]
+fn optionals() {
+    code_output_test(
+        "Optional",
+        r##"
+            [
+                {
+                    "in_both": 5,
+                    "missing": 5,
+                    "has_null": 5
+                },
+                {
+                    "in_both": 5,
+                    "has_null": null,
+                    "added": 5
+                }
+            ]
+        "##,
+        r##"
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct Optional {
+                in_both: i64,
+                missing: Option<i64>,
+                has_null: Option<i64>,
+                added: Option<i64>,
+            }
+        "##
+    );
+}
+
+#[test]
+fn fallback() {
+    code_output_test(
+        "Fallback",
+        r##"
+            [
+                {
+                    "only_null": null,
+                    "conflicting": 5,
+                    "empty_array": []
+                },
+                {
+                    "only_null": null,
+                    "conflicting": "five",
+                    "empty_array": []
+                }
+            ]
+        "##,
+        r##"
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct Fallback {
+                only_null: ::serde_json::Value,
+                conflicting: ::serde_json::Value,
+                empty_array: Vec<::serde_json::Value>,
+            }
+        "##
+    );
+}
+
+#[test]
+fn nesting() {
+    code_output_test(
+        "Root",
+        r##"
+            [
+                {
+                    "nested": { "a": 5 },
+                    "in_array": [{ "b": 5 }]
+                }
+            ]
+        "##,
+        r##"
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct Root {
+                nested: Nested,
+                in_array: Vec<InArray>,
+            }
+
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct InArray {
+                b: i64,
+            }
+
+            #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+            struct Nested {
+                a: i64,
+            }
+        "##
+    );
+}
