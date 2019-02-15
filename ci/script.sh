ba54943aceada0aadd280d899c7ec14c6c31c6e0
Erik Vesteraas <erik@vestera.as>
2019-02-15 21:31:51 +0100

    Release v0.3.2

--- ci/script.sh
+++ ci/script.sh
@@ -5,20 +5,20 @@ set -ex
 DIR="$(cd "$(dirname $0)/.." && pwd)"
 
 cd "$DIR/json_typegen_shared"
-cargo build --verbose
-cargo test --verbose
+cargo build --target "$TARGET" --verbose
+cargo test --target "$TARGET" --verbose
 
 cd "$DIR/json_typegen_cli"
-cargo build --verbose
-cargo test --verbose
+cargo build --target "$TARGET" --verbose
+cargo test --target "$TARGET" --verbose
 
 cd "$DIR/json_typegen_web"
-cargo build --verbose
-cargo test --verbose
+cargo build --target "$TARGET" --verbose
+cargo test --target "$TARGET" --verbose
 
 cd "$DIR/json_typegen"
-cargo build --verbose
-cargo test --verbose
+cargo build --target "$TARGET" --verbose
+cargo test --target "$TARGET" --verbose
 
 cd "$DIR/json_typegen_demo"
-cargo run --verbose
+cargo run --target "$TARGET" --verbose
