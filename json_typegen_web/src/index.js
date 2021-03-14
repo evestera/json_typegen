2e77b49881d5a53a8b6f90d1e23f303950ea3b33
Erik Vesteraas <erik@vestera.as>
2021-03-14 02:29:43 +0100

    web: Don't put large files in textarea

--- json_typegen_web/src/index.js
+++ json_typegen_web/src/index.js
@@ -5,13 +5,12 @@ import {restoreParams, storeParams} from "./localstorage";
 
 restoreParams();
 
+let largeInput;
+
 polyfill().then(() => import("../../json_typegen_wasm/pkg")).then(typegen_wasm => {
   const render = () => {
     const typename = $('typename').value;
-    let input = $('input').value;
-    if (input === '') {
-      input = '{}';
-    }
+    let input = largeInput || $('input').value;
     const options = ({
       output_mode: $('outputmode').value,
       property_name_format: $('propertynameformat').value,
@@ -32,12 +31,12 @@ polyfill().then(() => import("../../json_typegen_wasm/pkg")).then(typegen_wasm =
       typename,
       input: (input.length < 1000000) ? input : "",
       options,
-      extraoptions: extraoptions_json
+      extraoptions: extraoptions ? extraoptions_json : undefined
     })
 
     const combinedOptions = Object.assign({}, options, extraoptions || {});
 
-    const result = typegen_wasm.run(typename, input, JSON.stringify(combinedOptions));
+    const result = typegen_wasm.run(typename, input || "{}", JSON.stringify(combinedOptions));
 
     const target = $('target');
     target.value = result.trim();
@@ -59,11 +58,27 @@ polyfill().then(() => import("../../json_typegen_wasm/pkg")).then(typegen_wasm =
     if (file) {
       const reader = new FileReader();
       reader.onload = (fileEvent) => {
-        $('input').value = fileEvent.target.result;
+        const contents = fileEvent.target.result;
+        if (file.size > 1000000) {
+          largeInput = contents;
+          $('input').value = "";
+          $('large-file-overlay').classList.remove("is-invisible");
+          const fileSizeMb = (file.size / 1000000).toFixed(2);
+          $('large-file-message').textContent = `"${file.name}" (${fileSizeMb} MB)`
+        } else {
+          $('input').value = contents;
+        }
         render();
       }
       reader.readAsText(file);
     }
+  }
+
+  $('clear-input-button').onclick = () => {
+    largeInput = undefined;
+    $('large-file-overlay').classList.add("is-invisible");
+    $('input').value = "";
+    render();
   }
 
   render();
