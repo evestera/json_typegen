65d71c63675035532be7eb768115f1740fc0ffc7
Erik Vesteraas <erik@vestera.as>
2022-08-27 15:36:38 +0200

    WIP for fixing to_singular

--- json_typegen_shared/src/to_singular.rs
+++ json_typegen_shared/src/to_singular.rs
@@ -1,17 +1,60 @@
+// Words (especially ending in s) that are already singular and should be left as is
+const ALREADY_SINGULAR: &[&str] = &[
+    "series",
+    "species",
+    "news",
+    "clothes",
+];
+
+// (ends_with, strip_chars, append)
 const ENDS_WITH_RULES: &[(&str, usize, &str)] = &[
-    ("series", 0, ""),
+    ("zes", 3, ""),
+    ("matrices", 4, "ix"),
+    ("appendices", 4, "ix"),
+    ("radices", 4, "ix"),
+    ("vertices", 4, "ex"),
+    ("vortices", 4, "ex"),
+    ("indices", 4, "ex"),
+    ("codices", 4, "ex"),
+    ("people", 5, "erson"),
     ("cookies", 1, ""),
     ("movies", 1, ""),
+    ("ives", 4, "ife"),
+    ("lves", 4, "lf"),
+    ("rves", 4, "rf"),
+    ("buses", 2, ""),
+    ("gasses", 3, ""),
+    ("children", 3, ""),
+    ("feet", 3, "oot"),
+    ("leaves", 3, "f"),
+    ("staves", 3, "ff"),
+    ("thieves", 3, "f"),
+    ("fishes", 2, ""),
+    ("taxies", 2, ""),
     ("ies", 3, "y"),
+    ("oes", 2, ""),
     ("les", 1, ""),
     ("pes", 1, ""),
     ("ss", 0, ""),
-    ("es", 0, ""),
-    ("is", 0, ""),
+    ("sses", 2, ""),
+    ("yses", 2, "is"),
+    ("diagnoses", 2, "is"),
+    ("prognoses", 2, "is"),
+    ("synopses", 2, "is"),
+    ("crises", 2, "is"),
+    ("theses", 2, "is"),
+    ("statuses", 2, ""),
+    ("aliases", 2, ""),
+    ("ses", 1, ""),
+    ("xes", 2, ""),
+    ("schemas", 1, ""),
     ("as", 0, ""),
-    ("us", 0, ""),
+    ("es", 1, ""),
+    ("taxis", 1, ""),
+    ("is", 0, ""),
     ("os", 0, ""),
-    ("news", 0, ""),
+    ("menus", 1, ""),
+    ("us", 0, ""),
     ("s", 1, ""),
 ];
 
@@ -21,8 +64,17 @@ const ENDS_WITH_RULES: &[(&str, usize, &str)] = &[
 /// - Prefer to be conservative; Missing singularizations are better than incorrect ones.
 /// - It's OK if this is somewhat use-case specific. It's not exposed.
 /// - No regexes, since we don't want the regex dependency in the WASM.
+/// - We generally want to avoid replacing entire words, so that we don't need complex logic for preserving case
+///
+/// Known issues:
+/// -
 pub fn to_singular(s: &str) -> String {
     let lowercase = s.to_ascii_lowercase();
+    for suffix in ALREADY_SINGULAR {
+        if lowercase.ends_with(suffix) {
+            return s.to_string()
+        }
+    }
     for (suffix, to_strip, replacement) in ENDS_WITH_RULES {
         if lowercase.ends_with(suffix) {
             return s[0..(s.len() - to_strip)].to_string() + replacement;
@@ -36,7 +88,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_to_singular() {
+    fn test_to_singular_from_file() {
         let mut incorrectly_singularized: Vec<(&'static str, &'static str, String)> = Vec::new();
         let mut not_singularized: Vec<(&'static str, &'static str)> = Vec::new();
         let mut assert_to_singular_matches = |input: &'static str, expected: &'static str| {
@@ -50,43 +102,23 @@ mod tests {
             }
         };
 
-        assert_to_singular_matches("cards", "card");
-        assert_to_singular_matches("types", "type");
-        assert_to_singular_matches("colors", "color");
-        assert_to_singular_matches("rulings", "ruling");
-        assert_to_singular_matches("foreignNames", "foreignName");
-        assert_to_singular_matches("tags", "tag");
-        assert_to_singular_matches("categoryKeys", "categoryKey");
-        assert_to_singular_matches("attributes", "attribute");
-        assert_to_singular_matches("values", "value");
-        assert_to_singular_matches("images", "image");
+        let plurals_text = include_str!("to_singular_tests.txt");
+        // let plurals_text = include_str!("plurals.txt");
+        for line in plurals_text.lines() {
+            if line.trim().is_empty() { continue }
+            let split = line.split(' ').collect::<Vec<_>>();
+            let plural = split[0];
+            let singular = split[1];
+            assert_to_singular_matches(plural, singular);
+        }
 
-        assert_to_singular_matches("guesses", "guess");
-
-        assert_to_singular_matches("moves", "move");
-        assert_to_singular_matches("lives", "life");
-        assert_to_singular_matches("leaves", "leaf");
-
-        assert_to_singular_matches("legalities", "legality");
-        assert_to_singular_matches("abilities", "ability");
-        assert_to_singular_matches("queries", "query");
-        assert_to_singular_matches("cookies", "cookie");
-        assert_to_singular_matches("movies", "movie");
-
-        assert_to_singular_matches("matrices", "matrix");
-        assert_to_singular_matches("vertices", "vertex");
-        assert_to_singular_matches("indices", "index");
-        assert_to_singular_matches("slices", "slice");
-
-        assert_to_singular_matches("children", "child");
-
-        assert_to_singular_matches("series", "series");
-        assert_to_singular_matches("news", "news");
-        assert_to_singular_matches("axis", "axis");
+        if !not_singularized.is_empty() || !incorrectly_singularized.is_empty() {
+            println!("Incorrect: {}, Missed: {}\n", incorrectly_singularized.len(), not_singularized.len());
+        }
 
         if !not_singularized.is_empty() {
             println!(
-                "Missed {} singularizations for to_singular() (input, expected):\n  {}\n\n",
+                "Missed {} singularizations for to_singular() (input, expected):\n  {}\n",
                 not_singularized.len(),
                 not_singularized
                     .iter()
@@ -98,7 +130,8 @@ mod tests {
 
         if !incorrectly_singularized.is_empty() {
             panic!(
-                "Test failures for to_singular() (input, expected, output):\n  {}\n\n",
+                "Test failures ({}) for to_singular() (input, expected, output):\n  {}\n",
+                incorrectly_singularized.len(),
                 incorrectly_singularized
                     .iter()
                     .map(|(input, expected, output)| format!("{}, {}, {}", input, expected, output))
