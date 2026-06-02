227a8dbf282d82e806d69dbd493104b8e04d2215
Erik Vesteraas <erik@vestera.as>
2026-06-02 11:30:35 +0200

    wasm: Remove wee_alloc
    wee_alloc is unmaintained, and in practice is worse than the default
    Rust allocator. As suggested by @ink-splatters in PR #44, we could have
    switched to lol_alloc, but the code size difference is negligible for
    this project, so trade-off seems not worth it, so just removed wee_alloc
    instead.

--- json_typegen_wasm/src/lib.rs
+++ json_typegen_wasm/src/lib.rs
@@ -12,15 +12,6 @@ cfg_if! {
     }
 }
 
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
-
 #[wasm_bindgen]
 pub fn run(name: &str, input: &str, options: &str) -> String {
     set_panic_hook();
